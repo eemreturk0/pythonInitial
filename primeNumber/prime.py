ec4340a45809008eb1144f276607c21bc026b766
turke <turkemre234@gmail.com>
2022-08-04 12:09:56 +0300

    Prime Number completed

--- primeNumber/prime.py
+++ primeNumber/prime.py
@@ -42,3 +42,20 @@ print(divmod(10,6))      (1,4)
 #     print(a)
 #     a += 1
 """
+
+
+sayi=int(input("Sayıyı girin :"))
+if sayi > 1:
+    for i in range(2,sayi):
+        if(sayi%i)==0:
+            print(sayi,"Asal sayi degildir.")
+            break
+        else:
+            print(sayi,"Asal sayıdır.")
+    else:
+        print(sayi,"Asal sayı degildir.")
+
+
+
+
+
