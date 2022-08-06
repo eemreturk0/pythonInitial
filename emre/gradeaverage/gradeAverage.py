7574d0a639d3532ff93cc0938f0d5cab53b23a4a
turke <turkemre234@gmail.com>
2022-08-06 13:31:53 +0300

    not ortalaması gönderimi.

---
+++ emre/gradeaverage/gradeAverage.py
@@ -0,0 +1,15 @@
+print("**************")
+print()
+print("\t Not Ortalaması Bulma Python \t")
+print()
+print("**************")
+
+not1 = int(input("1.Notunuzu girin :"))
+not2 = int(input("2.Noutunuzu girin :"))
+not3 = int(input("3.Notunuzu girin :"))
+proje = int(input("Proje Notunuzu girin :"))
+ortalama = not1+not2+not3+proje
+
+sonuc=ortalama/4
+
+print("Not ortalamanız : ",sonuc)
